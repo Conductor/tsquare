29de376919d4ac5302be92ac2ad5104773e9e8a1
James Royalty <jroyalty@gmail.com>
2013-08-01 15:07:23 -0400

    Moved "controller" package into "web" namespace.

--- src/main/java/net/opentsdb/contrib/tsquare/web/controller/GraphiteController.java
+++ src/main/java/net/opentsdb/contrib/tsquare/web/controller/GraphiteController.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.opentsdb.contrib.tsquare.controller;
+package net.opentsdb.contrib.tsquare.web.controller;
 
 import java.io.IOException;
 import java.util.Set;
